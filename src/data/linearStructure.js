a35b3bc419837b0daba9a6d1f2a0ce8b8045c9e2
Lcheck <znfhdl789@naver.com>
2022-08-23 22:51:39 +0900

    Feat : 단순연결리스트 컨텐츠 추가 (설명, 문서, 강의, 예제) #27

--- src/data/linearStructure.js
+++ src/data/linearStructure.js
@@ -30,11 +30,16 @@ export const linearStructure = {
 
 					skills: [
 						{
-							name: "분기된 자료구조명(ex단순연결리스트)",
+							name: "단순연결리스트",
 							description: {
-								text: "text",
+								text: "단순연결리스트 (Singly Linked List) : 각 노드에 자료 공간과 한 개의 포인터 공간이 있고, 각 노드의 포인터는 다음 노드를 가리키는 자료구조",
 								links: [
-									["name", "html"],]}
+									["[문서] 자바로 단일 연결리스트 구현하기", "https://galid1.tistory.com/92"],
+									["[문서] C언어로 단일 연결리스트 구현하기", " https://happysalmon.tistory.com/74"],
+									["[강의] C언어로 단일 연결리스트 구현하기", "https://www.youtube.com/watch?v=3ZdafcIvREw"],
+									["[강의] 단일 연결리스트 분석", "https://www.youtube.com/watch?v=PtrlTt1hPJg"],
+									["[예제] 연결리스트를 활용해 특정 규칙의 에디터를 구현하는 예제", "https://www.youtube.com/watch?v=PtrlTt1hPJg"],
+									]}
 						},
 						{
 							name: "분기된 자료구조명(ex단순연결리스트)",
