32c4abcf405f85eed8c075c2ed90b5338ac69e3f
sohenney <ththgus0642@naver.com>
2022-08-23 16:35:07 +0900

    Feat : 정수 추가 (설명, 문서, 강의, 예제) #18

--- src/data/simpleStructure.js
+++ src/data/simpleStructure.js
@@ -10,12 +10,12 @@ export const simpleStructure = {
 					description: {
 						text: "정수 (Integer) : 양의 정수, 음의 정수 및 0으로 이루어진 수의 체계",
 						links: [
-							["설명", "https://ko.wikipedia.org/wiki/%EC%A0%95%EC%88%98"],
-							["[강의 1] 정수의 음수 표현 방식", "https://www.youtube.com/watch?v=Q1JQJp-X0S8"],
-							["[강의 2] 정수의 양수 표현 방식", "https://www.youtube.com/watch?v=P6s_66Ta72s"],
-							["[예제 1] 합이 0인 네 정수를 구하는 문제", "https://www.acmicpc.net/problem/7453"],
-							["[예제 2] 문자열 S에 포함된 정수의 개수를 구하는 문제", "https://www.acmicpc.net/problem/10821"],
-							["[예제 3] 정수 삼각형의 합을 구하는 문제 ", "https://www.acmicpc.net/problem/1932"],
+							["[문서] 정수의 정의, 성질", "https://ko.wikipedia.org/wiki/%EC%A0%95%EC%88%98"],
+							["[강의] 정수의 음수 표현 방식", "https://www.youtube.com/watch?v=Q1JQJp-X0S8"],
+							["[강의] 정수의 양수 표현 방식", "https://www.youtube.com/watch?v=P6s_66Ta72s"],
+							["[예제] 합이 0인 네 정수를 구하는 예제", "https://www.acmicpc.net/problem/7453"],
+							["[예제] 문자열 S에 포함된 정수의 개수를 구하는 예제", "https://www.acmicpc.net/problem/10821"],
+							["[예제] 정수 삼각형의 합을 구하는 예제", "https://www.acmicpc.net/problem/1932"],
 						]
 					}
 				},
