4c599e41c29c58381b5d70a5436449eba2287c79
sohenney <ththgus0642@naver.com>
2022-08-23 17:23:26 +0900

    Feat : 정수 컨텐츠 추가 (설명, 문서, 강의, 예제) #18

--- src/data/simpleStructure.js
+++ src/data/simpleStructure.js
@@ -2,15 +2,20 @@ export const simpleStructure = {
 	name: "simpleStructure",
 	areas: [
 		{
-			name: "자료구조명",
+			name: "정수",
 			skills: [
 
 				{
-					name: "자료구조명",
+					name: "정수",
 					description: {
-						text: "text",
+						text: "정수 (Integer) : 양의 정수, 음의 정수 및 0으로 이루어진 수의 체계",
 						links: [
-							["name", "html"],
+							["[문서] 정수의 정의, 성질", "https://ko.wikipedia.org/wiki/%EC%A0%95%EC%88%98"],
+							["[강의] 정수의 음수 표현 방식", "https://www.youtube.com/watch?v=Q1JQJp-X0S8"],
+							["[강의] 정수의 양수 표현 방식", "https://www.youtube.com/watch?v=P6s_66Ta72s"],
+							["[예제] 합이 0인 네 정수를 구하는 예제", "https://www.acmicpc.net/problem/7453"],
+							["[예제] 문자열 S에 포함된 정수의 개수를 구하는 예제", "https://www.acmicpc.net/problem/10821"],
+							["[예제] 정수 삼각형의 합을 구하는 예제", "https://www.acmicpc.net/problem/1932"],
 						]
 					}
 				},
