1ec96f561a4c10aafc5c092f219029d92fe6da21
sohenney <ththgus0642@naver.com>
2022-08-24 11:38:09 +0900

    이진트리 컨텐츠 추가 (설명, 문서, 강의, 예제) #32

--- src/data/nonLinearStructure.js
+++ src/data/nonLinearStructure.js
@@ -26,11 +26,17 @@ export const nonLinearStructure = {
 								]}
 						},
 						{
-							name: "분기된 자료구조명(ex단순연결리스트)",
+							name: "이진트리",
 							description: {
-								text: "text",
+								text: "이진트리 (Binary Tree) : 각각의 노드가 최대 두 개의 자식 노드를 가지는 트리 자료구조",
 								links: [
-									["name", "html"],]}
+									["[문서] 이진트리의 정의, 특징, 종류", "https://kingpodo.tistory.com/27"],
+									["[문서] 이진트리 순회 방식", "https://kingpodo.tistory.com/28?category=805745"],
+									["[강의] 이진트리의 구현과 순회 알고리즘", "https://youtu.be/z_tzHoPfllA"],
+									["[예제]  이진트리를 순회하는 예제", "https://www.acmicpc.net/problem/1991"],
+									["[예제] 이진 검색 트리의 전위 순회가 주어졌을 때 후위 순회를 구하는 예제", "https://www.acmicpc.net/problem/5639"],
+									["[예제] 완전 이진트리의 부분 집합을 찾는 예제", "https://www.acmicpc.net/problem/1634"],
+								]}
 						},
 					]
 				},
