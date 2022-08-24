47562dc015453717e18e8953ac0ede4f803b9195
lys <dlfjs4641@naver.com>
2022-08-24 11:34:07 +0900

    Feat : 그래프 컨텐츠 추가 (설명, 문서, 강의, 예제) #32

--- src/data/nonLinearStructure.js
+++ src/data/nonLinearStructure.js
@@ -38,21 +38,26 @@ export const nonLinearStructure = {
 			]
 		},
 		{
-			name: "분기될 자료구조명(ex연결리스트)",
+			name: "그래프",
 			skills: [
 
 				{
-					name: "분기될 자료구조명(ex연결리스트)",
+					name: "그래프",
 					description: {
-						text: "text",
+						text: "그래프 (Graph) : 이해를 돕기위해 단순화시킨 그림",
 						links: [
-							["name", "html"],
+							["[문서] 기술용어 해설", "http://www.ktword.co.kr/test/view/view.php?nav=2&no=3980&sh=%EA%B7%B8%EB%9E%98%ED%94%84"],
+							["[강의] Graphs - 그래프", "https://www.youtube.com/watch?v=QYbKqENoIAo"],
+							["[강의] 그래프에 대해서", "https://www.youtube.com/watch?v=fVcKN42YXXI"],
+							["[예제] 최소 횟수로 도로를 수정하는 그래프 예제", "https://www.acmicpc.net/problem/1119"],
+							["[예제] 수열을 출력하는 그래프 예제", "https://www.acmicpc.net/problem/1432"],
+							["[예제] 이분 그래프 예제", "https://www.acmicpc.net/problem/1707"],
 						]
 					},
 
 					skills: [
 						{
-							name: "분기된 자료구조명(ex단순연결리스트)",
+							name: "방향 그래프",
 							description: {
 								text: "text",
 								links: [
