9c999251c1dad5bb10e02dd16e89293404b25559
lys <dlfjs4641@naver.com>
2022-08-24 10:27:52 +0900

    Feat : 스택 컨텐츠 추가 (설명, 문서, 강의, 예제) #27

--- src/data/linearStructure.js
+++ src/data/linearStructure.js
@@ -61,17 +61,23 @@ export const linearStructure = {
 			]
 		},
 		{
-			name: "자료구조명",
+			name: "스택",
 			skills: [
 
 				
 
 				{
-					name: "자료구조명",
+					name: "스택",
 					description: {
-						text: "text",
+						text: "스택 (Stack) : 차곡차곡 쌓아둔 모양/형태를 의미"
+							 +"			      후입선출(LIFO) 리스트를 말함",
 						links: [
-							["name", "html"],
+							["[문서] 기술용어 해설", "http://www.ktword.co.kr/test/view/view.php?nav=2&no=1306&sh=%EC%8A%A4%ED%83%9D"],
+							["[강의] 스택 자료구조 5분만에 이해하기", "https://youtu.be/DsZHDmth6Pc"],
+							["[강의] 자료구조 스택과 그 응용", "https://youtu.be/D5k4wem7GQo"],
+							["[예제] pop, push 수행을 계산하는 스택 예제", "https://www.acmicpc.net/problem/1874"],
+							["[예제] 입력으로 명령을 저리하는 스택 예제", "https://www.acmicpc.net/problem/10828"],
+							["[예제] 정해진 기호로 게산하여 결과를 출력하는 스택 예제", "https://www.acmicpc.net/problem/3425"],
 						]
 					}
 				},
