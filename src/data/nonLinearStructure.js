75e293334a777ffdc231e47d8022e0fb59859da4
sohenney <ththgus0642@naver.com>
2022-08-24 11:30:35 +0900

    Feat : 일반트리 컨텐츠 추가 (설명, 문서, 강의, 예제) #32

--- src/data/nonLinearStructure.js
+++ src/data/nonLinearStructure.js
@@ -3,25 +3,27 @@ export const nonLinearStructure = {
 	areas: [
 		
 		{
-			name: "분기될 자료구조명(ex연결리스트)",
+			name: "트리",
 			skills: [
 
 				{
-					name: "분기될 자료구조명(ex연결리스트)",
+					name: "트리",
 					description: {
-						text: "text",
-						links: [
-							["name", "html"],
-						]
+						text: "트리 (Tree) : 방향성이 있는 비순환 그래프의 한 종류",
 					},
 
 					skills: [
 						{
-							name: "분기된 자료구조명(ex단순연결리스트)",
+							name: "일반트리",
 							description: {
-								text: "text",
+								text: "일반트리 (Tree) : 데이터들을 간선으로 연결한 계층형 자료구조",
 								links: [
-									["name", "html"],]}
+									["[문서] 트리 정의, 구조", "https://kingpodo.tistory.com/26?category=805745"],
+									["[강의] 트리 자료구조 10분 요약", "https://youtu.be/i5yHkP1jQmo"],
+									["[예제] 트리의 부모를 찾는 예제", "https://www.acmicpc.net/problem/11725"],
+									["[예제] 주어진 그래프가 트리인지 판별하는 예제", "https://www.acmicpc.net/problem/4803"],
+									["[예제] 트리 순회 순서를 구하는예제", "https://www.acmicpc.net/problem/2263"],
+								]}
 						},
 						{
 							name: "분기된 자료구조명(ex단순연결리스트)",
