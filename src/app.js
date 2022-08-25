3844937fdf697ea8d620e56db93f1830f2b7f62a
lys <dlfjs4641@naver.com>
2022-08-25 14:03:42 +0900

    Fix : GitHub 아이콘 수정 #58

--- src/app.js
+++ src/app.js
@@ -690,7 +690,7 @@ export class App extends LitElement {
 			</div>
 			<header id="header">
 				<div>
-					<a href="https://github.com/andreasbm/web-skills" target="_blank" rel="noopener" aria-label="Open Github" title="Open Github">
+					<a href="https://github.com/threelee/DataStructure-Master" target="_blank" rel="noopener" aria-label="Open Github" title="Open Github">
 						<ws-icon hoverable .template="${githubIconTemplate}"></ws-icon>
 					</a>
 					${user != null ? html`
